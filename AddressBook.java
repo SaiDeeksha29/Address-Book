dfc881eeb24886f61e8eeb24d7cf059b909a8817
SaiDeeksha29 <saideeksha29@gmail.com>
2020-09-27 18:35:14 +0530

    Modified:Edit the contact on Address Book

--- AddressBook.java
+++ AddressBook.java
@@ -22,4 +22,14 @@ public class AddressBook {
 		address.add(cobj);
 	}
 
+	public ArrayList<Contact> updateByFirstName(String first) {
+		ArrayList<Contact> obj = new ArrayList<Contact>();
+		for (int i = 0; i < address.size(); i++) {
+			if (address.get(i).getFirstName().contains(first)) {
+				obj.add(address.get(i));
+			}
+		}
+		return obj;
+	}
+
 }
