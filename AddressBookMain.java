813a235577418c449c96284bdebff805f911db96
SaiDeeksha29 <saideeksha29@gmail.com>
2020-09-27 17:50:14 +0530

    Added:Welcome Message

---
+++ AddressBookMain.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package com.capgemini.training.model;
+
+/**
+ * @author saideeksha
+ *
+ */
+public class AddressBookMain {
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to Address Book program in AddressBookMain class on Master branch");
+	}
+}
